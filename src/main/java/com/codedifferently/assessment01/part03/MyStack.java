58fe71f8c41c6cd44992c11611a896d65286bb30
BwinaRose <sabrinakrueger24@gmail.com>
2022-06-22 15:24:27 -0400

    finished part 3

--- src/main/java/com/codedifferently/assessment01/part03/MyStack.java
+++ src/main/java/com/codedifferently/assessment01/part03/MyStack.java
@@ -1,20 +1,36 @@
 package com.codedifferently.assessment01.part03;
 
+import java.util.ArrayList;
 import java.util.EmptyStackException;
+import java.util.List;
 
 public class MyStack<T>{
+    private List<T> stack = new ArrayList<T>();
 
-    public boolean isEmpty() { return false; }
-
-    public void push(T i) { }
-
-    public T pop() throws EmptyStackException {
-        return null;
+    public boolean isEmpty() {
+        return stack.isEmpty();
     }
 
-    public T peek() { return null; }
+    public void push(T i) {
+        stack.add(i);
+    }
+
+    public T pop() throws EmptyStackException {
+        if (stack.isEmpty()) {
+            EmptyStackException EmptyStackException = new EmptyStackException();
+            throw EmptyStackException;
+        }
+        T top = stack.get(stack.size() - 1);
+        stack.remove(stack.size() - 1);
+        return top;
+    }
+
+    public T peek() { if (stack.isEmpty()) {
+        return null;
+    }
+        return stack.get(stack.size() - 1);}
 
     public Integer size(){
-        return null;
+        return stack.size();
     }
 }
