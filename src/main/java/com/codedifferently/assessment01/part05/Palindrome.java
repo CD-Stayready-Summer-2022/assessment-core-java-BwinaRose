569d6362128e7846bcd932e529e3791c1a2d7408
BwinaRose <sabrinakrueger24@gmail.com>
2022-06-22 15:55:31 -0400

    finished part 5

--- src/main/java/com/codedifferently/assessment01/part05/Palindrome.java
+++ src/main/java/com/codedifferently/assessment01/part05/Palindrome.java
@@ -2,6 +2,27 @@ package com.codedifferently.assessment01.part05;
 
 public class Palindrome {
     public Integer countPalindromes(String input){
-        return null;
+        char[] charArray = input.toCharArray();
+        int lengthOfString = input.length();
+        int count = lengthOfString;
+        for (int i = 0; i < lengthOfString - 1; i++){
+            int left = i, right = i + 1;
+            while (left >= 0 && right < lengthOfString){
+                if (charArray[left] == charArray[right]){
+                    count++;
+                    left--;
+                    right++;
+                }else break;
+            }
+            left = i -1;
+            right = i + 1;
+            while (left >= 0 && right < lengthOfString){
+                if (charArray[left] == charArray[right]){
+                    count++;
+                    left--;
+                    right++;
+                }else break;
+            }
+        }return count;
     }
 }
