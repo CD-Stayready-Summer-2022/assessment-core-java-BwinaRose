fbdd2ef89238bac74eabfeb92553ea70a9609912
BwinaRose <sabrinakrueger24@gmail.com>
2022-06-22 15:28:27 -0400

    finished part 4

--- src/main/java/com/codedifferently/assessment01/part04/Jumper.java
+++ src/main/java/com/codedifferently/assessment01/part04/Jumper.java
@@ -3,9 +3,9 @@ package com.codedifferently.assessment01.part04;
 public class Jumper {
 
     public int jumps(int k, int j) {
-        int numOfJumps = k/j;
-        int numOfJumpsRemaining = k%j;
-        int totalJumps = numOfJumps+numOfJumpsRemaining;
+        int numOfJumps = k / j;
+        int numOfJumpsRemaining = k % j;
+        int totalJumps = numOfJumps + numOfJumpsRemaining;
         return totalJumps;
     }
 }
